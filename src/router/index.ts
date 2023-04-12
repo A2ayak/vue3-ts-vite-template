2d6972156ea66a863ce9cf1a3adfc3157b99dc0b
A2ayak <429853312@qq.com>
2023-04-12 17:02:40 +0800

    调整

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,8 +7,8 @@ const routes = [
 
 const router = createRouter({
 	// hash 或者 history 模式
-	// history: createWebHashHistory(),
-	history: createWebHistory(),
+	history: createWebHashHistory(),
+	// history: createWebHistory(),
 	routes,
 })
 // 全局路由守卫的方法跟vue-router3一样
@@ -18,7 +18,7 @@ router.beforeEach((to, from, next) => {
 	next()
 })
 //路由全局后置守卫
-router.afterEach((to, from, next) => {
+router.afterEach((to, from) => {
 	console.log('路由全局后置守卫', to, from)
 })
 
